45dd14421604f577befb6ad403890ad181174465
Green <fhrrud0334@gamil.com>
2021-10-07 17:10:58 +0900

    swagger size

--- src/main/java/com/example/reactionGame/dto/TestDto.java
+++ src/main/java/com/example/reactionGame/dto/TestDto.java
@@ -4,6 +4,9 @@ package com.example.reactionGame.dto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @ToString
@@ -11,6 +14,8 @@ import java.util.Date;
 @Setter
 public class TestDto {
 
+    @NotBlank
+    @Size(min = 10, max = 99999, message = "10이상 입력하세요")
     private Integer idx;
     private Date date;
     private String name;
