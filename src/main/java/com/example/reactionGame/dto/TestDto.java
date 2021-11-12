8a984a4219c735de4467330fa76fe18076499afe
Green <fhrrud0334@gamil.com>
2021-11-12 15:06:15 +0900

    Aws secret manager

---
+++ src/main/java/com/example/reactionGame/dto/TestDto.java
@@ -0,0 +1,20 @@
+package com.example.reactionGame.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import java.util.Date;
+
+@ToString
+@Getter
+@Setter
+public class TestDto {
+
+    private Integer idx;
+    private Date date;
+    private String name;
+    private Integer ratetime;
+
+
+}
