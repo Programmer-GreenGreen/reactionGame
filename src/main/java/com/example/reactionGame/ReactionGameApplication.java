43125420e53e89e776559e76314239a82cce1220
Green <fhrrud0334@gamil.com>
2021-11-30 15:45:12 +0900

    main

--- src/main/java/com/example/reactionGame/ReactionGameApplication.java
+++ src/main/java/com/example/reactionGame/ReactionGameApplication.java
@@ -1,11 +1,22 @@
 package com.example.reactionGame;
 
+//import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.example.reactionGame.cotroller.MemberController;
+import com.example.reactionGame.service.MemberService;
+import com.example.reactionGame.service.MemberServiceImpl;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ReactionGameApplication {
 
-
+	public static void main(String[] args) {
+		SpringApplication.run(ReactionGameApplication.class, args);
+	}
 }
