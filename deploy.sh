59a3e381bb3cf3fb588fb8eece5d814d3b5692d0
Green <fhrrud0334@gamil.com>
2021-12-04 16:08:26 +0900

    nohup

--- deploy.sh
+++ deploy.sh
@@ -21,4 +21,4 @@ else
 fi
 
 echo "> $JAR_PATH 배포" #3
-nohup java -jar /home/ec2-user/app/build/libs/reactionGame-0.0.1-SNAPSHOT.jar --spring.config.location=/home/ec2-user/app/appspec.yml > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar /home/ec2-user/app/build/libs/reactionGame-0.0.1-SNAPSHOT.jar
